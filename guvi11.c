b2b0ae7b47f5a43e1a17a0c1ca6a5f2c91f25c44
jeroomej <35652011+jeroomej@users.noreply.github.com>
2018-02-06 15:46:47 +0530

    Create guvi11.c

---
+++ guvi11.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the base number");
+scanf("%d",&a);
+printf("enter the power");
+scanf("%d",&b);
+c=pow(a,b);
+printf("the value is %d',c);
+}
