c4fbf183676d120f451664e919a8554dcc0e5b8d
jeroomej <35652011+jeroomej@users.noreply.github.com>
2018-02-22 16:31:53 +0530

    Create guvi38.c

---
+++ guvi38.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int array[10],n,i;
+printf("enter the size of the azrray");
+scanf("%d",&n);
+printf("\nenter the array elements");
+for(i=0;i,n;i++)
+{
+scanf("%d",&array[i]);
+printf(\n%d %d,array[i],i);
+}
+return 0;
+}
