a38a05a226a087b82876d8ee1244502f51b8f437
jeroomej <35652011+jeroomej@users.noreply.github.com>
2018-03-08 16:44:20 +0530

    Update guvi52.c

--- guvi52.c
+++ guvi52.c
@@ -6,34 +6,37 @@ printf("enter the number");
 scanf("%d",&a);
 switch(a)
 {
-case 1;
+  case 1:
 printf("\none");
 break;
-case 2;
+  case 2:
 printf("\ntwo");
 break;
-case 3;
+  case 3:
 printf("\nthree");
 break;
-case 4;
+  case 4:
 printf("\nfour");
 break;
-case 5;
+  case 5:
 printf("\nfive");
 break;
-case 6;
+  case 6:
 printf("\nsix");
 break;
-case 7;
+  case 7:
 printf("\nseven");
 break;
-case 8;
+  case 8:
 printf("\neight");
 break;
-case 9;
+  case 9:
 printf("\nnine");
 break;
-case 10;
+  case 10:
 printf("\nten");
 break;
+  default:
+    printf("enter the numbers between 1 and 10");
+}
 }
