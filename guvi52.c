9d789635083934d9b8a105d04da9f0a9b5f5653a
jeroomej <35652011+jeroomej@users.noreply.github.com>
2018-03-08 16:33:48 +0530

    Create guvi52.c

---
+++ guvi52.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+switch(a)
+{
+case 1;
+printf("\none");
+break;
+case 2;
+printf("\ntwo");
+break;
+case 3;
+printf("\nthree");
+break;
+case 4;
+printf("\nfour");
+break;
+case 5;
+printf("\nfive");
+break;
+case 6;
+printf("\nsix");
+break;
+case 7;
+printf("\nseven");
+break;
+case 8;
+printf("\neight");
+break;
+case 9;
+printf("\nnine");
+break;
+case 10;
+printf("\nten");
+break;
+}
