2553f0b4ad3f28c9f8ea16a4fd6eee2f09ea0291
jeroomej <35652011+jeroomej@users.noreply.github.com>
2018-03-08 16:20:22 +0530

    Update guvi51.c

--- guvi51.c
+++ guvi51.c
@@ -2,21 +2,21 @@
 
 int main(void) 
 {
-	int num,rem,rev=0;
+	int n,r,r=0;
 	printf("enter the number:");
 	scanf("%d",&num);
 	while(num!=0)
 	{
-		rem=num%10;
-		rev=(rev*10)+rem;
-		num=num/10;
+		r=n%10;
+		r=(r*10)+r;
+		n=n/10;
 	}
-	num=rev;
-	while(num!=0)
+	n=r;
+	while(n!=0)
 	{
-		rem=num%10;
-		printf("%d ",rem);
-		num=num/10;
+		r=n%10;
+		printf("%d ",r);
+		n=n/10;
 	}
 	return 0;
 
