8b7d9d799b9cf87dcc48b7cd6f06f730c74b807e
jeroomej <35652011+jeroomej@users.noreply.github.com>
2018-03-15 16:13:40 +0530

    Create guvi63.c

---
+++ guvi63.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	char str[20];
+	int i,l=1;
+	printf("enter the sentence:");
+	scanf("%[^\n]s",str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(str[i]==' ')
+		{
+			l=l+1;
+		}
+	}
+	printf("\nno of words:%d",l);
+}
